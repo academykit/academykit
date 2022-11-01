3e04bfa82c83c71ea199606240bd56b3ece45a9c
sursabipul <bipul.thapa@sursatech.com>
2022-11-01 13:49:57 +0545

    folder structure

---
+++ src/Domain/Enums/UserActivityType.cs
@@ -0,0 +1,45 @@
+﻿namespace Lingtren.Domain.Enums
+{
+    public enum UserActivityType
+    {
+        LogIn = 1,
+        LogOut= 2,
+        UserCreated = 3,
+        UserUpdated = 4,
+        UserMarkedInactive =5,
+        ResetPassword = 6,
+        ChangePassword = 7,
+
+        CreateCourse = 11,
+        UpdateCourse =12,
+        DeleteCourse = 13,
+        ArchiveCourse =14,
+        EnrolledCourse = 15,
+        UnEnrolledCourse = 16,
+        CompletedCourses = 17,
+        AddedTeacher = 18,
+        RemoveTeacher = 19,
+
+        CreateLiveSession = 21,
+        UpdatedLiveSession = 22,
+        DeleteLiveSession  = 23,
+        ArchiveLiveSession = 24,
+        EnrolledLiveSession = 25,
+        UnEnrolledLiveSession = 26,
+        JoinedLiveSession = 27,
+        LeftLiveSession = 28,
+        AddedModerator = 29,
+        RemoveModerator = 30,
+
+        CreateExam = 31,
+        UpdateExam = 32,
+        DeleteExam = 33,
+        ArchiveExam = 34,
+        MarkCompletedExam = 35,
+        EnrolledExam = 36,
+        UnEnrolledExam = 37,
+        AttemptedExam = 38,
+
+        SettingsUpdated = 41,
+    }
+}
