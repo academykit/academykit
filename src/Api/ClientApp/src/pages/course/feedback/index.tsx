0e066d13decf67618dc0288d930e4487fd4b1c91
atprabesh <prabesh@vurilo.com>
2023-01-26 13:44:37 +0545

    fix: no questions found

--- src/Api/ClientApp/src/pages/course/feedback/index.tsx
+++ src/Api/ClientApp/src/pages/course/feedback/index.tsx
@@ -1,16 +1,63 @@
 import FeedbackForm from "@components/Course/FeedBack/ViewFeedback";
-import { Container, Divider } from "@mantine/core";
+import { Title, Button, Container, createStyles } from "@mantine/core";
 
 import { useFeedbackQuestion } from "@utils/services/feedbackService";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: 80,
+    paddingBottom: 80,
+  },
+
+  title: {
+    fontWeight: 900,
+    fontSize: 34,
+    marginBottom: theme.spacing.md,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 32,
+    },
+  },
+
+  control: {
+    [theme.fn.smallerThan("sm")]: {
+      width: "100%",
+    },
+  },
+}));
 
 const FeedbackPage = () => {
   const { id } = useParams();
+  const { classes } = useStyles();
 
   const feedback = useFeedbackQuestion(id as string, "");
+  const navigate = useNavigate();
+
+  if (feedback.data && feedback.data?.length < 1) {
+    return (
+      <Container className={classes.root}>
+        <div>
+          <Title className={classes.title}>No questions found...</Title>
+
+          <Button
+            variant="outline"
+            size="md"
+            mt="xl"
+            onClick={() => navigate(-1)}
+            className={classes.control}
+          >
+            Go back
+          </Button>
+        </div>
+      </Container>
+    );
+  }
 
   return (
     <Container my={50}>
+      hi
       {feedback.isSuccess && (
         <FeedbackForm item={feedback.data} lessonId={id as string} />
       )}
