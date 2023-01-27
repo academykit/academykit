63b156524ea283c4020a039b35bb5a46810194d1
atprabesh <prabesh@vurilo.com>
2023-01-27 09:49:30 +0545

    fix: readable badge for lessons

--- src/Api/ClientApp/src/utils/enums.ts
+++ src/Api/ClientApp/src/utils/enums.ts
@@ -31,6 +31,9 @@ export enum FeedbackType {
 export enum ReadableEnum {
   SingleChoice = "Single Choice",
   MultipleChoice = "Multiple Choice",
+  LiveClass = "Live Class",
+  RecordedVideo= "Recorded Video "
+
 }
 
 export enum LessonType {
