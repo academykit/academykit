37ee65c8ff05b84cb123b98c68704a37de2f0fac
atprabesh <prabesh@vurilo.com>
2023-06-11 13:46:13 +0545

    fix: STANDALONE #18

--- src/Api/ClientApp/src/pages/pool/index.tsx
+++ src/Api/ClientApp/src/pages/pool/index.tsx
@@ -38,7 +38,7 @@ const useStyle = createStyles((theme) => ({
 const schema = () => {
   const { t } = useTranslation();
   return Yup.object().shape({
-    name: Yup.string().required(t("name_required") as string),
+    name: Yup.string().required(t("pool_name_required") as string),
   });
 };
 
