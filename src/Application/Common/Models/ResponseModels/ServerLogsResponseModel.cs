2f325cf04ce10fd28f3eae0087feb18ef31a4341
iambijay12 <bijay.dhimal@sursatech.com>
2023-07-07 10:58:04 +0545

    fix: server log api

---
+++ src/Application/Common/Models/ResponseModels/ServerLogsResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using Lingtren.Domain.Enums;
+
+namespace Lingtren.Application.Common.Models.ResponseModels
+{
+    public class ServerLogsResponseModel
+    {
+        public Guid Id { get; set; }
+        public SeverityType Type { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Message { get; set; }
+        public string TrackBy { get; set; }
+    }
+}
