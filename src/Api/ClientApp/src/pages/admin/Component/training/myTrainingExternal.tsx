f9068c8ab0e87a64a3454124d7f188e628feed3b
Bidhan Bajracharya <137468252+bidhan-vurilo@users.noreply.github.com>
2023-08-08 14:33:43 +0545

    fix: trash color red

--- src/Api/ClientApp/src/pages/admin/Component/training/myTrainingExternal.tsx
+++ src/Api/ClientApp/src/pages/admin/Component/training/myTrainingExternal.tsx
@@ -287,7 +287,7 @@ const MyTrainingExternal = () => {
                           setDeleteCertificate(true);
                         }}
                       >
-                        <IconTrash />
+                        <IconTrash color="red" />
                       </ActionIcon>
                     </>
                   )}
