0801273e4fb632bf993adcd57c614cbc5bcf1eff
AamitShakya <145314505+AamitShakya@users.noreply.github.com>
2024-02-21 17:42:49 +0545

    FEAT:Given Data for AI

---
+++ src/Application/Common/Models/ResponseModels/AiResponseMode.cs
@@ -0,0 +1,8 @@
+namespace Lingtren.Application.Common.Models.ResponseModels
+{
+    public class AiResponseMode
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
