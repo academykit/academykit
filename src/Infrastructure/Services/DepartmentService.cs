84fa3a5cab9e90ffd4f926e030454237e49be9e3
AamitShakya <145314505+AamitShakya@users.noreply.github.com>
2024-02-23 11:28:54 +0545

    FIX:Question Order

--- src/Infrastructure/Services/DepartmentService.cs
+++ src/Infrastructure/Services/DepartmentService.cs
@@ -18,15 +18,17 @@
     using Microsoft.Extensions.Logging;
     using Microsoft.IdentityModel.Tokens;
 
-
-    public class DepartmentService : BaseGenericService<Department, DepartmentBaseSearchCriteria>, IDepartmentService
+    public class DepartmentService
+        : BaseGenericService<Department, DepartmentBaseSearchCriteria>,
+            IDepartmentService
     {
         public DepartmentService(
             IUnitOfWork unitOfWork,
             ILogger<DepartmentService> logger,
-            IStringLocalizer<ExceptionLocalizer> localizer) : base(unitOfWork, logger, localizer)
-        {
-        }
+            IStringLocalizer<ExceptionLocalizer> localizer
+        )
+            : base(unitOfWork, logger, localizer) { }
+
         #region Protected Methods
         /// <summary>
         /// This is called before entity is saved to DB.
@@ -37,7 +39,11 @@
         protected override async Task CreatePreHookAsync(Department entity)
         {
             await CheckDuplicateDepartmentNameAsync(entity).ConfigureAwait(false);
-            entity.Slug = CommonHelper.GetEntityTitleSlug<Department>(_unitOfWork, (slug) => q => q.Slug == slug, entity.Name);
+            entity.Slug = CommonHelper.GetEntityTitleSlug<Department>(
+                _unitOfWork,
+                (slug) => q => q.Slug == slug,
+                entity.Name
+            );
             await Task.FromResult(0);
         }
 
@@ -47,7 +53,10 @@
         /// <remarks>Override in child services to update navigation properties.</remarks>
         /// <param name="existing">The existing entity.</param>
         /// <param name="newEntity">The new entity.</param>
-        protected override async Task UpdateEntityFieldsAsync(Department existing, Department newEntity)
+        protected override async Task UpdateEntityFieldsAsync(
+            Department existing,
+            Department newEntity
+        )
         {
             await CheckDuplicateDepartmentNameAsync(newEntity).ConfigureAwait(false);
             _unitOfWork.GetRepository<Department>().Update(newEntity);
@@ -59,7 +68,10 @@
         /// <param name="predicate">The predicate.</param>
         /// <param name="criteria">The search criteria.</param>
         /// <returns>The updated predicate with applied filters.</returns>
-        protected override Expression<Func<Department, bool>> ConstructQueryConditions(Expression<Func<Department, bool>> predicate, DepartmentBaseSearchCriteria criteria)
+        protected override Expression<Func<Department, bool>> ConstructQueryConditions(
+            Expression<Func<Department, bool>> predicate,
+            DepartmentBaseSearchCriteria criteria
+        )
         {
             if (!string.IsNullOrWhiteSpace(criteria.Search))
             {
@@ -98,7 +110,9 @@
         /// </summary>
         /// <param name="query">The query.</param>
         /// <returns>The updated query.</returns>
-        protected override IIncludableQueryable<Department, object> IncludeNavigationProperties(IQueryable<Department> query)
+        protected override IIncludableQueryable<Department, object> IncludeNavigationProperties(
+            IQueryable<Department> query
+        )
         {
             return query.Include(x => x.User);
         }
@@ -112,19 +126,30 @@
         {
             return p => p.Id.ToString() == identity || p.Slug == identity;
         }
+
         /// <summary>
         /// Check if entity could be deleted
         /// </summary>
         /// <param name="entityToDelete">The entity being deleted</param>
-        protected override async Task CheckDeletePermissionsAsync(Department entityToDelete, Guid CurrentUserId)
+        protected override async Task CheckDeletePermissionsAsync(
+            Department entityToDelete,
+            Guid CurrentUserId
+        )
         {
-            var existUsers = await _unitOfWork.GetRepository<User>().ExistsAsync(
-                predicate: p => p.DepartmentId == entityToDelete.Id
-                ).ConfigureAwait(false);
+            var existUsers = await _unitOfWork
+                .GetRepository<User>()
+                .ExistsAsync(predicate: p => p.DepartmentId == entityToDelete.Id)
+                .ConfigureAwait(false);
             if (existUsers)
             {
-                _logger.LogWarning("Department with id: {id} contains users so it cannot be deleted for user with id: {userId}.", entityToDelete.Id, CurrentUserId);
-                throw new ForbiddenException(_localizer.GetString("DepartmentContainsUsersCannotDeleted"));
+                _logger.LogWarning(
+                    "Department with id: {id} contains users so it cannot be deleted for user with id: {userId}.",
+                    entityToDelete.Id,
+                    CurrentUserId
+                );
+                throw new ForbiddenException(
+                    _localizer.GetString("DepartmentContainsUsersCannotDeleted")
+                );
             }
         }
         #endregion Protected Methods
@@ -138,11 +163,19 @@
         /// <exception cref="ServiceException"></exception>
         private async Task CheckDuplicateDepartmentNameAsync(Department entity)
         {
-            var departmentExist = await _unitOfWork.GetRepository<Department>().ExistsAsync(
-                predicate: p => p.Id != entity.Id && p.Name.ToLower() == entity.Name.ToLower()).ConfigureAwait(false);
+            var departmentExist = await _unitOfWork
+                .GetRepository<Department>()
+                .ExistsAsync(
+                    predicate: p => p.Id != entity.Id && p.Name.ToLower() == entity.Name.ToLower()
+                )
+                .ConfigureAwait(false);
             if (departmentExist)
             {
-                _logger.LogWarning("Duplicate department name : {name} is found for the department with id : {id}.", entity.Name, entity.Id);
+                _logger.LogWarning(
+                    "Duplicate department name : {name} is found for the department with id : {id}.",
+                    entity.Name,
+                    entity.Id
+                );
                 throw new ServiceException(_localizer.GetString("DuplicateDepartmentNameFound"));
             }
         }
@@ -155,25 +188,41 @@
         /// <param name="searchCriteria">the instance of <see cref="BaseSearchCriteria"/></param>
         /// <param name="currentUserId">the current logged in user id</param>
         /// <returns></returns>
-        public async Task<SearchResult<UserResponseModel>> GetUsers(string identity, BaseSearchCriteria searchCriteria, Guid currentUserId)
+        public async Task<SearchResult<UserResponseModel>> GetUsers(
+            string identity,
+            BaseSearchCriteria searchCriteria,
+            Guid currentUserId
+        )
         {
-            var department = await GetByIdOrSlugAsync(identity, currentUserId).ConfigureAwait(false);
+            var department = await GetByIdOrSlugAsync(identity, currentUserId)
+                .ConfigureAwait(false);
 
             var predicate = PredicateBuilder.New<User>(true);
-            predicate = predicate.And(p => p.DepartmentId == department.Id && p.Status == UserStatus.Active);
+            predicate = predicate.And(
+                p => p.DepartmentId == department.Id && p.Status == UserStatus.Active
+            );
 
             if (!string.IsNullOrWhiteSpace(searchCriteria.Search))
             {
                 var search = searchCriteria.Search.ToLower().Trim();
-                predicate = predicate.And(x =>
-                    ((x.FirstName.Trim() + " " + x.MiddleName.Trim()).Trim() + " " + x.LastName.Trim()).Trim().Contains(search)
-                 || x.Email.ToLower().Trim().Contains(search)
-                 || x.MobileNumber.ToLower().Trim().Contains(search));
+                predicate = predicate.And(
+                    x =>
+                        (
+                            (x.FirstName.Trim() + " " + x.MiddleName.Trim()).Trim()
+                            + " "
+                            + x.LastName.Trim()
+                        )
+                            .Trim()
+                            .Contains(search)
+                        || x.Email.ToLower().Trim().Contains(search)
+                        || x.MobileNumber.ToLower().Trim().Contains(search)
+                );
             }
 
-            var users = await _unitOfWork.GetRepository<User>().GetAllAsync(
-                predicate: predicate
-                ).ConfigureAwait(false);
+            var users = await _unitOfWork
+                .GetRepository<User>()
+                .GetAllAsync(predicate: predicate)
+                .ConfigureAwait(false);
 
             var searchResult = users.ToIPagedList(searchCriteria.Page, searchCriteria.Size);
 
@@ -186,59 +235,81 @@
                 TotalCount = searchResult.TotalCount
             };
 
-            searchResult.Items.ForEach(p =>
-                  response.Items.Add(new UserResponseModel(p))
-              );
+            searchResult.Items.ForEach(p => response.Items.Add(new UserResponseModel(p)));
             return response;
         }
 
         /// <summary>
-        /// Get Department 
+        /// Get Department
         /// </summary>
         /// <param name ="departmentName">the group id or slug</param>
         /// <returns>the instance of <see cref="UserResponseModel"/></returns>
-        public async Task<List<UserResponseModel>> GetUserByDepartmentName(Guid CurrentUserId, string departmentName)
+        public async Task<List<UserResponseModel>> GetUserByDepartmentName(
+            Guid CurrentUserId,
+            string departmentName
+        )
         {
             try
             {
-                var user = await _unitOfWork.GetRepository<User>().GetFirstOrDefaultAsync(predicate: p => p.Id == CurrentUserId);
-                if (user.Role == UserRole.SuperAdmin || user.Role == UserRole.Admin || user.Role == UserRole.Trainer)
+                var user = await _unitOfWork
+                    .GetRepository<User>()
+                    .GetFirstOrDefaultAsync(predicate: p => p.Id == CurrentUserId);
+                if (
+                    user.Role == UserRole.SuperAdmin
+                    || user.Role == UserRole.Admin
+                    || user.Role == UserRole.Trainer
+                )
                 {
-                    var department = await _unitOfWork.GetRepository<Department>().GetAllAsync(predicate: p => p.Name == departmentName,
-                        include: src => src.Include(x => x.Users)).ConfigureAwait(false);
+                    var department = await _unitOfWork
+                        .GetRepository<Department>()
+                        .GetAllAsync(
+                            predicate: p => p.Name == departmentName,
+                            include: src => src.Include(x => x.Users)
+                        )
+                        .ConfigureAwait(false);
                     var resopnse = new List<UserResponseModel>();
-                    foreach (var departmentUser in department.SelectMany(x => x.Users.Where(x => x.Role != UserRole.Admin && x.Role != UserRole.SuperAdmin)))
+                    foreach (
+                        var departmentUser in department.SelectMany(
+                            x =>
+                                x.Users.Where(
+                                    x => x.Role != UserRole.Admin && x.Role != UserRole.SuperAdmin
+                                )
+                        )
+                    )
                     {
-                        resopnse.Add(new UserResponseModel
-                        {
-                            Id = departmentUser.Id,
-                            FirstName = departmentUser.FirstName,
-                            MiddleName = departmentUser.MiddleName,
-                            LastName = departmentUser.LastName,
-                            Email = departmentUser.Email,
-                            MobileNumber = departmentUser.MobileNumber,
-                            Role = departmentUser.Role,
-                            Profession = departmentUser.Profession,
-                            Address = departmentUser.Address,
-                            Bio = departmentUser.Bio,
-                            ImageUrl = departmentUser.ImageUrl,
-                            PublicUrls = departmentUser.PublicUrls,
-                            Status = departmentUser.Status,
-                            CreatedOn = departmentUser.CreatedOn,
-                            FullName = departmentUser.FullName,
-                            DepartmentId = departmentUser.DepartmentId,
-                            DepartmentName = departmentUser.Department?.Name
-                        });
+                        resopnse.Add(
+                            new UserResponseModel
+                            {
+                                Id = departmentUser.Id,
+                                FirstName = departmentUser.FirstName,
+                                MiddleName = departmentUser.MiddleName,
+                                LastName = departmentUser.LastName,
+                                Email = departmentUser.Email,
+                                MobileNumber = departmentUser.MobileNumber,
+                                Role = departmentUser.Role,
+                                Profession = departmentUser.Profession,
+                                Address = departmentUser.Address,
+                                Bio = departmentUser.Bio,
+                                ImageUrl = departmentUser.ImageUrl,
+                                PublicUrls = departmentUser.PublicUrls,
+                                Status = departmentUser.Status,
+                                CreatedOn = departmentUser.CreatedOn,
+                                FullName = departmentUser.FullName,
+                                DepartmentId = departmentUser.DepartmentId,
+                                DepartmentName = departmentUser.Department?.Name
+                            }
+                        );
                     }
 
                     return resopnse;
                 }
                 else
                 {
-                    throw new UnauthorizedAccessException(_localizer.GetString("TrainerNotAllowedDepartmentUser"));
+                    throw new UnauthorizedAccessException(
+                        _localizer.GetString("TrainerNotAllowedDepartmentUser")
+                    );
                 }
             }
-
             catch
             {
                 throw new UnauthorizedAccessException(_localizer.GetString("CannotAccess"));
