2d154e577f2bdda0c402291193a72c175544b389
Sailesh Bajgain <sailesh.bajgain@sursatech.com>
2024-09-10 16:51:19 +0545

    fix: conflict solved

--- AcademyKit.Server/Infrastructure/Persistence/Migrations/20240910110130_Initial.Designer.cs
+++ AcademyKit.Server/Infrastructure/Persistence/Migrations/20240910110130_Initial.Designer.cs
@@ -4,6 +4,7 @@ using AcademyKit.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AcademyKit.Server.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240910110130_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
