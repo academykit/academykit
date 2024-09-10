8dee4a3b303c6b3b968b832a446dba90656a2230
sursabipul <bipul.thapa@sursatech.com>
2024-09-10 11:48:31 +0545

    fix: format issue

--- AcademyKit.Server/Application/Common/Models/RequestModels/UserRoleRequestModel.cs
+++ AcademyKit.Server/Application/Common/Models/RequestModels/UserRoleRequestModel.cs
@@ -1,6 +1,7 @@
 ﻿using AcademyKit.Domain.Enums;
 
 namespace AcademyKit.Application.Common.Models.RequestModels;
+
 public class UserRoleRequestModel
 {
     public required UserRole Role { get; set; }
